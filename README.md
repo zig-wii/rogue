8b3d2666a0d61be452eceff8339b1552bb61a910
Knarkzel <knarkzel@gmail.com>
2022-06-10 17:09:12 +0200

    Proper instructions

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 2D platformer/rpg game made with [zogc](https://github.com/zig-wii/zogc).
 
 ```
-git clone https://github.com/zig-wii/zrogue
+git clone --recursive https://github.com/zig-wii/zrogue
 cd zrogue/
 zig build run
 ```
--- build.zig
+++ build.zig
@@ -2,21 +2,6 @@ const std = @import("std");
 const zogc = @import("vendor/zogc/build.zig");
 
 pub fn build(builder: *std.build.Builder) !void {
-    try ensure_packages(builder.allocator, "vendor");
-    var obj = try zogc.target_wii(builder, .{ .name = "main" });
-    obj.addPackagePath("zogc", "./vendor/zogc/src/main.zig");
-}
-
-// Ensures packages in folder are installed with git submodule
-fn ensure_packages(allocator: std.mem.Allocator, path: []const u8) !void {
-    const dir = try std.fs.cwd().openDir(path, .{ .iterate = true });
-    var iter = dir.iterate();
-    while (try iter.next()) |entry| {
-        const target = try std.fmt.allocPrint(allocator, "{s}/{s}", .{ path, entry.name });
-        var child = std.ChildProcess.init(&.{ "git", "submodule", "update", "--init", "--recursive", target }, allocator);
-        child.cwd = std.fs.path.dirname(@src().file) orelse ".";
-        child.stderr = std.io.getStdErr();
-        child.stdout = std.io.getStdOut();
-        _ = try child.spawnAndWait();
-    }
+    var obj = try zogc.target_wii(builder, .{ .name = "main", .textures = "src/textures" });
+    obj.addPackagePath("zogc", "vendor/zogc/src/main.zig");
 }
